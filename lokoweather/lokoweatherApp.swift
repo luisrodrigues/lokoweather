8fffcb1cdd4bfe121840ab0bc5b05798eb942040
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-19 19:38:06 +0000

    Initial Commit

---
+++ lokoweather/lokoweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  lokoweatherApp.swift
+//  lokoweather
+//
+//  Created by Rodrigues, Luis (EXTERN: EEDS) on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct lokoweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
