887c2f5d24a40cc9b0a6dcaa0b8ca90d0262e288
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-19 22:16:24 +0000

    Finished implementing UI part

--- lokoweather/ContentView.swift
+++ lokoweather/ContentView.swift
@@ -11,16 +11,46 @@ struct ContentView: View {
     var body: some View {
         ZStack {
 			LinearGradient(
-				gradient: Gradient(colors: [Color.blue, Color.white]),
+				gradient: Gradient(colors: [.blue, Color("lightBlue")]),
 				startPoint: .topTrailing,
 				endPoint: .bottomTrailing
 			)
 			.edgesIgnoringSafeArea(.all)
 			VStack {
-				Text("Porto, PT")
+				Text("Porto")
 					.font(.system(size: 32, weight: .medium, design: .default))
 					.foregroundColor(.white)
 					.padding(.bottom)
+				VStack(spacing: 10) {
+					Image(systemName: "cloud.sun.fill")
+						.renderingMode(.original)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width: 180, height: 180)
+					Text("16º")
+						.font(.system(size: 70, weight: .medium))
+						.foregroundColor(.white)
+						.padding(.bottom)
+				}
+				.padding(.bottom, 40)
+				
+				HStack(spacing: 20) {
+					WeatherDayView(dayOfWeek: "TUE", imageName: "wind", temperature: 16)
+					WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 16)
+					WeatherDayView(dayOfWeek: "THU", imageName: "cloud.fog.fill", temperature: 16)
+					WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.rain.fill", temperature: 16)
+					WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.fill", temperature: 16)
+				}
+				Spacer()
+				Button {
+					print("tapped")
+				} label: {
+					Text("Change Day Time")
+						.frame(width: 280, height: 50)
+						.background(Color.white)
+						.font(.system(size: 20, weight: .bold, design: .default))
+						.cornerRadius(10)
+				}
 				Spacer()
 			}
         }
@@ -31,4 +61,27 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct WeatherDayView: View {
+	var dayOfWeek: String
+	var imageName: String
+	var temperature: Int
+	
+	var body: some View {
+		VStack {
+			Text(dayOfWeek)
+				.font(.system(size: 16, weight: .medium, design: .default))
+				.foregroundColor(.white)
+			Image(systemName: imageName)
+				.renderingMode(.original)
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(width: 40, height: 40)
+			Text("\(temperature)º")
+				.font(.system(size: 20, weight: .medium))
+				.foregroundColor(.white)
+				.padding(.bottom)
+		}
+	}
 }
